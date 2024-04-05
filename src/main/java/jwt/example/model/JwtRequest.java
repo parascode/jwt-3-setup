6f5bd231a1932f1b1e744c1be4f1f3358917db81
parascode <jparas435@gmail.com>
2024-04-05 22:44:11 +0530

    jwt authentication with inMemory

---
+++ src/main/java/jwt/example/model/JwtRequest.java
@@ -0,0 +1,14 @@
+package jwt.example.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class JwtRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/jwt/example/model/JwtResponse.java
@@ -0,0 +1,14 @@
+package jwt.example.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+public class JwtResponse {
+    private String jwtToken;
+    private String username;
+}
